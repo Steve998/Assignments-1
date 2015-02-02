6f48acce5f8fa1a24d093ef96c93f590f318bf92
Diana Davis <dianadryerdavis@yahoo.com>
2015-02-01 19:12:13 -0500

    got the loops working, but still working on messages

--- assignments/assignment13.rb
+++ assignments/assignment13.rb
@@ -14,38 +14,38 @@ loop do
   puts "Let's play a game. I'm thinking of a number between 1 and ... \n\nActually, I'll let you choose the range. Please choose the second number. \n\nRemember, if it's really big, you'll be guessing for a long time!\n\n"
 
   top_of_range = gets.chomp.to_i
-
-  puts "\n\nThanks! Okay, I'm thinking of a number between (and including) 1 and #{top_of_range}. Can you guess it?\n\n"
-
   # Generates a random integer between (and including) 1 and whatever number the user chose.
   my_number = rand(1..top_of_range)
-
   # Sets counter to 0.
   how_many = 0
 
-  # Starts guessing
-  guess = gets.chomp.to_i
+  puts "\n\nThanks! Okay, I'm thinking of a number between (and including) 1 and #{top_of_range}. Can you guess it?\n\n"
 
-    loop do
+  guess = 0
+  until guess == my_number do
+
+    # Starts guessing
+    guess = gets.chomp.to_i
     how_many += 1
 
-    # Describes three levels of accuracy and displays messages to the user.
-    if how_many == 1
-      puts "Wowsa! You got it on the first try! \n\nYou should pay the lottery!"
-    elsif how_many == my_number
-      puts "At last! You finally got it ... after eliminating all other possibilities. :) \n\nYou got it in #{how_many} out of #{how_many} guesses."
-    else puts "That's it! Nice job! You got it in #{how_many} guesses."
+    # Message for user who doesn't follow game rules
+    if guess > top_of_range || guess < 1
+      puts "Remember, you set the range of 1 to #{top_of_range}. Please guess a number within that range."
     end
 
-  # Need a way to check if guess is outside the range
-  #  else puts "Sorry, not quite."
-    if guess >= my_number
+    # Describes levels of accuracy and displays messages to the user.
+    if guess == my_number && how_many == 1
+      puts "Wowsa! You got it on the first try! \n\nYou should pay the lottery!"
+    elsif guess == my_number && how_many == my_number
+      puts "At last! You finally got it ... after eliminating all other possibilities. :) \n\nYou got it in #{how_many} out of #{how_many} guesses."
+    elsif guess == my_number
+      puts "That's it! Nice job! You got it in #{how_many} guesses."
+    elsif guess > my_number
       puts "Too high. Please guess again."
     else puts "Too low. Please guess again."
-
     end
 
-    break if guess == my_number
+  end
 
   puts "Want to play again? y/n"
   again = gets.chomp
@@ -53,7 +53,5 @@ loop do
     if again == "n"
       break
     end
-
-  end
 
 end
