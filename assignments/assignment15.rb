e7f86cf96013166c6584532cce9e07555d13947b
Diana Davis <dianadryerdavis@yahoo.com>
2015-02-02 13:15:23 -0500

    fixes file setup; deletes extraneous variable

--- assignments/assignment15.rb
+++ assignments/assignment15.rb
@@ -1,11 +1,10 @@
-=begin
-This is assignment 15. We did it in class on Feb 02, 2015.
+=begin This is assignment 15 and is due on Feb 02, 2015.
 
-Alter assignment 12 so the array of questions comes from an outside file.
-The rest of the code shouldn't be changed.
-
-Iterate over the array using each, asking each question to the user
-Bonus: Save all the answers with the questions, and print a summary at the end
+Copy and then modify assignment 12 to read the questions from a file instead of the hard coded array
+Load them into an array so the rest of the program can remain the same
+If your assignment 12 is coded correctly, you won't have to change the part that asks questions
+Can be done with two lines of code
+Remember to have the text file of questions in the same directory where your Ruby code is
 =end
 
 #!/usr/bin/ruby
@@ -17,7 +16,8 @@ name = gets.chomp
 puts "\nNice to meet you, #{name.capitalize}. I'd like to ask you a few questions."
 
 # This section is new for this assignment.
-questions = File.new("assignment15_questions.txt")
+file = File.new("assignment15_questions.txt")
+questions = file.readlines
 
 # This section was moved to file "assignment15_questions.txt"
 #questions = ["Are you a night owl or an early riser?",
@@ -30,8 +30,9 @@ all_answers = Array.new
 
 questions.each do |question|
   puts "\n#{question}\n"
-  answer = gets.chomp
-  all_answers << answer
+  #answer = gets.chomp
+  #all_answers << answer
+  all_answers << gets.chomp
 end
 
 puts "\n\nWow, that was an informative interview! Thanks! Here's a transcript:\n\n"
