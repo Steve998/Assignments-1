87cf176aa3dd7219741a39207e455d661ccce086
Diana Davis <dianadryerdavis@yahoo.com>
2015-02-03 17:10:59 -0500

    first commit

--- assignments/assignment20.rb
+++ assignments/assignment20.rb
@@ -1,35 +1,70 @@
 =begin
-This is assignment 19 and is due on Feb 04, 2015.
+This is assignment 20 and is due on Feb 04, 2015.
 
-Extend Ruby's String class to have a "pigatize" method that will turn any string into pig latin.
+Note: Please check in your Gemfile and Gemfile.lock
+write a Ruby script that uses the Rails ActiveSupport gem, installed via a Gemfile and bundler
+the script should do the following when run:
+1) Show the date of the beginning of the current week
+2) Show what the date will be in 30 days
+3) Show what day of the week today's date will fall on next year
+4) Show what day of the week the user's birthday will be on this year
+5) Using the String class, pluralize a singular word the user enters and show the singular for a plural word the user enters
+6) Using the String class, truncate a phrase the user enters to 20 letters
+7) Using the Array class, convert an array of three words to a sentence with one command
 
-Should use the same code you wrote before, but now you are extending the String class and putting the functionality in there.
+You'll need the following Rails extensions that come with ActiveSupport:
+http://api.rubyonrails.org/v4.0.3/classes/Date.html
+http://api.rubyonrails.org/v4.0.3/classes/String.html
+http://api.rubyonrails.org/v4.0.3/classes/Array.html
+http://api.rubyonrails.org/v4.0.3/classes/Numeric.html
 =end
 
 #!/usr/bin/ruby
+require 'active_support'
+require 'active_support/all'
 
-# This extends the String class to include the constant VOWELS.
-String::VOWELS = %w(a e i o u)
+puts "Hi, Brian.\n\n"
 
-# This adds "pigatize" method to the String class.
-class String
+# Shows the date of the beginning of the current week
+puts "The date of the beginning of the current week: \n\n#{Date.today.beginning_of_week}.\n\n"
 
-    def pigatize(passed_text)
+# Shows what the date will be in 30 days
+puts "The date in 30 days: \n\n#{Date.today+30}.\n\n"
 
-      # Check to see if the first letter is a vowel, if not it's a consonant
-      if passed_text.starts_with_vowel(passed_text[0])
-        # Action if word starts with a vowel
-        pigatized_text = passed_text + "way"
-      else
-        # Action if word starts with a consonant
-        pigatized_text = passed_text.reverse.chop.reverse + passed_text[0] + "ay"
-      end
-      return pigatized_text
-    end
+# Shows what day of the week today's date will fall on next year
+puts "Today is #{Date.today}. In #{Date.today.year+1}, #{Date.today.strftime("%B" "%e")} will fall on a #{Date.today.advance(:years => +1).strftime("%A")}.\n\n"
 
-    # Check to see if the first letter is a vowel
-    def starts_with_vowel(passed_text)
-      self.VOWELS.include? (passed_text[0]).downcase
-    end
+# Shows what day of the week the user's birthday will be on this year
+puts "Oh, I'm sorry. I should invite you to play, too.\n\n"
+puts "If you tell me your birthday, I'll tell you what day of the week that falls on this year.\n\n"
+puts "When is your birthday? Please enter it like this: MM/DD.\n\n"
+birthday = Date.parse(gets.chomp)
+puts "\nIn #{Date.today.year} your birthday falls on a #{birthday.strftime("%A")}."
 
+# Using the String class, pluralizes a singular word the user enters and shows the singular for a plural word the user enters
+puts "\nPlease enter a singular word. Anything but 'campus'!\n\n"
+singular_word = gets.chomp
+if singular_word == "campus"
+  puts "\nWeisenheimer. Here is the INCORRECT plural of your word: #{singular_word.pluralize}.\n\n"
+else
+  puts "\nHere is the plural of your word: #{singular_word.pluralize}.\n\n"
 end
+
+# Using the String class, truncates a phrase the user enters to 20 letters
+puts "Please enter a phrase and I'll cut it down to 20 characters.\n\n"
+phrase = gets.chomp
+puts "\n#{phrase.truncate(20)}\n\n"
+
+# Using the Array class, converts an array of three words to a sentence with one command
+puts "If you enter three words I'll turn them into a sentence with one command. Ready?"
+words = Array.new
+
+loop do
+  puts "\nPlease enter a single word.\n\n"
+  words << gets.chomp
+  break if words.size == 3
+end
+
+puts "\nVoila! \n\n#{words.to_sentence}"
+
+puts "\nThanks for playing!"
